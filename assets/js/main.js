64d4a1187c6bcf8009010dd4c32c2c9e450a105e
Diego Silva <ds.dondeese@gmail.com>
2023-06-13 15:46:02 -0400

    new formula button copy code

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const copyButtonLabel = "Copia el Código";
+
+// use a class selector if available
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+  // only add button if browser supports Clipboard API
+  if (navigator.clipboard) {
+    let button = document.createElement("button");
+    button.classList.add("language-javascript-copy")
+
+    button.innerText = copyButtonLabel;
+    block.appendChild(button);
+
+    button.addEventListener("click", async () => {
+      await copyCode(block, button);
+    });
+  }
+});
+
+async function copyCode(block, button) {
+  let code = block.querySelector("code");
+  let text = code.innerText;
+
+  await navigator.clipboard.writeText(text);
+
+  // visual feedback that task is completed
+  button.innerText = "Código Copiado";
+
+  setTimeout(() => {
+    button.innerText = copyButtonLabel;
+  }, 700);
+}
